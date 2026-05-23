9a6ac89cbd648e9d9566c6ed0f72e6f29618732f
BrunoPs03 <pscheidtbruno@gmail.com>
2026-05-22 21:29:16 -0300

    Update Main.java
    Fazendo alterações na Main - Bruno Correa Pscheidt

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,23 +1,42 @@
-package app; // pacote do Main
+package app;
 
-import modelo.Veiculo; 
-import modelo.Carro;
-import modelo.Moto;
+import java.util.Scanner;
+
+import Modelo.Veiculo;
+import Modelo.Carro;
+import Modelo.Moto;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("=== Sistema de Gerenciamento de Estacionamento ===\n");
 
-        // ===== Criando objetos concretos =====
-        Carro civic = new Carro("ABC-1234", "Honda Civic", 4);
-        Moto cb = new Moto("XYZ-9876", "Honda CB 300", 300);
-        
-        Veiculo v1 = civic;
-        Veiculo v2 = cb;
-       
-        
-        System.out.println("--- Tarifa para 2 horas (120 minutos) ---");
-        System.out.println(v1 + " → R$ " + String.format("%.2f", v1.calcularTarifa(120)));
-        System.out.println(v2 + " → R$ " + String.format("%.2f", v2.calcularTarifa(120)));
-    }
-}
+        System.out.println("Escolha o tipo de veículo:");
+        System.out.println("1 - Carro");
+        System.out.println("2 - Moto");
+
+        int opcao = sc.nextInt();
+        sc.nextLine(); // limpar buffer
+
+        Veiculo veiculo;
+
+        System.out.print("Digite a placa: ");
+        String placa = sc.nextLine();
+
+        System.out.print("Digite o modelo: ");
+        String modelo = sc.nextLine();
+
+        if (opcao == 1) {
+
+            System.out.print("Digite o número de portas: ");
+            int portas = sc.nextInt();
+
+            veiculo = new Carro(placa, modelo, portas);
+
+        } else if (opcao == 2) {
+
+            System.out.print("Digite as cilindradas: ");
+            int cilindradas = sc.nextInt();
